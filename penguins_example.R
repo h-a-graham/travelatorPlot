b4de8c05a0c908406ee5c89f8c13a6a81810c43c
Hugh Graham <hugh.a.graham@gmail.com>
2022-02-04 14:59:05 +0000

    hk

--- penguins_example.R
+++ penguins_example.R
@@ -2,6 +2,7 @@ library(palmerpenguins)
 library(ggplot2)
 library(dplyr)
 library(tidyr)
+library(ggdist)
 #load functions
 source('barcodeBoxPlot.R')
 
@@ -18,7 +19,7 @@ penguins %>%
 
 # build base plot with the barcode
 p <- barcodeBoxPlot(penguins, species, bill_length_mm,
-                    .gapnudge=.38, .alpharange=c(0.1,.6)) +
+                    .gapnudge=.35, .alpharange=c(0.1,.6)) +
   theme_light()+
   labs(x='Species', y='Bill Length (mm)') +
   guides(fill="none", alpha='none')+
@@ -36,15 +37,48 @@ ggsave('exports/bill_length_mm.png', width=8.47, height=7.47)
 
 # with raincloud overlay
 p +
-  ggdist::stat_halfeye(adjust = .5, width = .3, .width = 0, justification = -.4,
-    point_colour = 'NA', slab_fill=NA, slab_colour='black', slab_alpha = .3,
+  ggdist::stat_halfeye(adjust = .3, width = .3, .width = 0, justification = -.3,
+    point_colour = 'NA', slab_fill=NA, slab_colour='black',
     slab_size=0.4) +
+  ggdist::stat_gradientinterval(width=5)+
   geom_boxplot(width = .15, outlier.shape = NA, fill=NA) +
-  geom_point(size = 1.3, alpha=0.3, position = position_jitter(
-    seed = 1, width = .07)) +
+  ggdist::stat_dots(
+    side = "left",
+    dotsize = .8,
+    justification = 1.15,
+    binwidth = .3,
+    colour='black'
+  ) +
+  # geom_point(size = 1.3, alpha=0.3, position = position_jitter(
+  #   seed = 1, width = .07)) +
   coord_cartesian(xlim = c(0.6, NA), clip = "off")
 
 ggsave('exports/bill_length_mm2.png', width=8.47, height=7.47)
+
+# the right way:
+penguins %>%
+  ggplot(aes(x = species, y = bill_length_mm, fill = species)) +
+  stat_gradientinterval(adjust=0.7,position = "dodge", colour=NA, width=0.9) +
+  theme_light()+
+  labs(x='Species', y='Bill Length (mm)') +
+  guides(fill="none", alpha='none')+
+  theme(axis.title.y = element_text(margin = margin(t = 0, r = 10, b = 0, l = 0)),
+        axis.title.x = element_text(margin = margin(t = 10, r = 0, b = 0, l = 0)))+
+  scale_fill_brewer(palette = 'Dark2') +
+  ggdist::stat_halfeye(adjust = .3, width = .3, .width = 0, justification = -.3,
+                       point_colour = 'NA', slab_fill=NA, slab_colour='black',
+                       slab_size=0.7) +
+  geom_boxplot(width = .15, outlier.shape = NA, fill=NA) +
+  ggdist::stat_dots(
+    side = "left",
+    dotsize = .8,
+    justification = 1.15,
+    binwidth = .3,
+    colour='black'
+  ) +
+  coord_cartesian(xlim = c(1, NA), clip = "off")
+
+ggsave('exports/bill_length_mm3.png', width=8.47, height=7.47)
 
 
 
